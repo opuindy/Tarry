f28e83fcbead9b85e92f432f1e8f078fb1bab472
Tarry <opuindy@gmail.com>
2023-07-25 19:50:28 +0100

    treated image path bug

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,17 @@
 import SUCCESSICON from './successIcon.png';
 import ERRORICON from './ErrorIcon.png';
+import AUDIOPHILE from './audiophile.png';
+import DEVJOB from './devjob.png';
+import INVOICEAPP from './invoiceApp.png';
+import VOREM from './vorem.png';
+import KANBAN from './KANBAN.png';
 
-export { SUCCESSICON, ERRORICON };
+export {
+  SUCCESSICON,
+  AUDIOPHILE,
+  ERRORICON,
+  DEVJOB,
+  INVOICEAPP,
+  VOREM,
+  KANBAN,
+};
--- src/utilities/works.js
+++ src/utilities/works.js
@@ -1,3 +1,11 @@
+import {
+  AUDIOPHILE,
+  DEVJOB,
+  INVOICEAPP,
+  KANBAN,
+  VOREM,
+} from '../assets/images';
+
 export const projectWorks = [
   {
     id: 1,
@@ -5,8 +13,8 @@ export const projectWorks = [
     slug: 'kanban',
     about: `Manage your tasks efficiently with Kanban, the customizable project management tool. With a visual board, customizable columns and statuses, Kanban helps you stay organized and productive. Plus, with both light and dark theme modes, you can work comfortably no matter the lighting conditions. Prioritize and manage tasks with ease, and keep track of your workload effortlessly with Kanban.`,
     technology: ['Javascript', 'Nextjs', 'Redux', 'SCSS'],
-    desktopImage: '/src/assets/images/KANBAN.png',
-    mobileImage: '/src/assets/images/KANBAN.png',
+    desktopImage: `${KANBAN}`,
+    mobileImage: `${KANBAN}`,
     liveUrl: 'https://tarry-kanban.vercel.app/',
     gitUrl: 'https://github.com/opuindy/Kanban',
     client: 'personal',
@@ -18,8 +26,8 @@ export const projectWorks = [
     slug: 'audiophile',
     about: `Upgrade your audio experience with Audiophile, the ultimate e-commerce destination for music enthusiasts. Browse high-quality audio products, add to your cart, and easily checkout with our streamlined process. With multiple payment and delivery options, it's never been easier to enhance your listening pleasure. Shop now with Audiophile and discover the difference in sound quality!`,
     technology: ['Javascript', 'React', 'Styled Component'],
-    desktopImage: '/src/assets/images/audiophile.png',
-    mobileImage: '/src/assets/images/audiophile.png',
+    desktopImage: `${AUDIOPHILE}`,
+    mobileImage: `${AUDIOPHILE}`,
     liveUrl: 'https://audiophile-e-commerce-webapp-tarry.vercel.app/',
     gitUrl: 'https://github.com/opuindy/AudioPhile-E-Commerce-Site',
     client: 'personal',
@@ -30,8 +38,8 @@ export const projectWorks = [
     slug: 'vorem',
     about: `Vorem Academy Landing Page is a sleek and modern clone of vorem academy blockchain learning platform. The landing page showcases the platform's features and benefits, including its comprehensive course offerings, user-friendly interface, and experienced instructors. With a clean design, Vorem Academy Landing Page is an engaging and effective way to promote the platform and attract new users.`,
     technology: ['Javascript', 'React', 'SCSS'],
-    desktopImage: '/src/assets/images/vorem.png',
-    mobileImage: '/src/assets/images/vorem.png',
+    desktopImage: `${VOREM}`,
+    mobileImage: `${VOREM}`,
     liveUrl: 'https://vorem-landing-page.vercel.app/',
     gitUrl: 'https://github.com/opuindy/Vorem-academy-Landing-Page',
     client: 'personal',
@@ -43,8 +51,8 @@ export const projectWorks = [
     about:
       'Streamline your invoicing process with this user-friendly web application. Create, manage, and track invoices with ease, including the ability to save drafts and mark pending invoices as paid. Filter invoices by status (draft/pending/paid) to stay on top of your financials. Perfect for small business owners, freelancers, and anyone looking to simplify their invoicing.',
     technology: ['Javascript', 'React', 'Styled Component'],
-    desktopImage: '/src/assets/images/invoiceApp.png',
-    mobileImage: '/src/assets/images/invoiceApp.png',
+    desktopImage: `${INVOICEAPP}`,
+    mobileImage: `${INVOICEAPP}`,
     liveUrl: 'https://invoice-app-tarry.vercel.app/',
     gitUrl: 'https://github.com/opuindy/Invoice-App',
     client: 'personal',
@@ -56,8 +64,8 @@ export const projectWorks = [
     about:
       'Find your perfect tech job anywhere in the world with Devjobs, the ultimate job board for techies. Search for jobs that match your skills, preferences, and location, or browse the curated selection to find your dream job. With the ability to apply for jobs directly through the website, landing your next job has never been easier. Start your job search today with Devjobs, and take the next step in your tech career!',
     technology: ['Javascript', 'React', 'Styled Component'],
-    desktopImage: '/src/assets/images/devjob.png',
-    mobileImage: '/src/assets/images/devjob.png',
+    desktopImage: `${DEVJOB}`,
+    mobileImage: `${DEVJOB}`,
     liveUrl: 'https://dev-jobs-webapp-tarry.vercel.app/',
     gitUrl: 'https://github.com/opuindy/DevJobs-webApp',
     client: 'personal',
